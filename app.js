6900b7d5c72c57a14d993f0f810f2fc4c14474f9
MobileGameServer <mobilegameserver@gmail.com>
2017-12-08 21:13:15 +0900

    clean up

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 
 'use strict'
 
+let cfg = require('./cfg')
 let express = require('express')
 let path = require('path')
-
 let logger = require('morgan')
-
-let bodyParser = require('body-parser')
 let cookieParser = require('cookie-parser')
 let cookieSession = require('cookie-session')
+let bodyParser = require('body-parser')
+let aws = require('aws-sdk')
 
 express.response.releaseRedirect = function (dbConn, url) {
   dbConn.release()
@@ -42,12 +42,38 @@ express.response.releaseSend = function (dbConn, message) {
 
 let app = express()
 app.disable('x-powered-by')
+app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal'])
 
+app.set('cfg', cfg)
 
 // view engine setup
-app.set('trust proxy', ['loopback', 'linklocal', 'uniquelocal'])
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+let mysql = require('mysql')
+let db000 = mysql.createPool(cfg.db000)
+let db001 = mysql.createPool(cfg.db001)
+app.set('db000', db000)
+app.set('db001', db001)
+
+/*
+let redis = require('redis')
+let rd000 = redis.createClient(cfg.rd000.port, cfg.rd000.host)
+let rd001 = redis.createClient(cfg.rd001.port, cfg.rd001.host)
+app.set('rd000', rd000)
+app.set('rd001', rd001)
+*/
+
+aws.config.update({
+    region: cfg.aws.region_name,
+    endpoint: cfg.dynamodb.endpoint_url
+})
+
+let ddbTable = new aws.DynamoDB()
+app.set('ddbTable', ddbTable)
+
+let ddbItem = new aws.DynamoDB.DocumentClient()
+app.set('ddbItem', ddbItem)
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
